4a5b9a33e74499048e2788b481e16b04595a9915
Bigcream <96058142+Bigcream@users.noreply.github.com>
2022-10-11 23:16:37 +0700

    refactor and config DB

--- src/main/java/com/example/learn/infrastructure/config/RedisConfig.java
+++ src/main/java/com/example/learn/infrastructure/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.learn.config;
+package com.example.learn.infrastructure.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
---
+++ src/main/java/com/example/learn/infrastructure/database/entity/UserEntity.java
@@ -0,0 +1,29 @@
+package com.example.learn.infrastructure.database.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    //getter setters
+}
